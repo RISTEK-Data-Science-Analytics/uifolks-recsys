32fb1755b6ee7354864ca815b880d748dd0a43fd
Belati <belatijagadbintangsyuhada@gmail.com>
2024-09-07 01:08:56 +0700

    Define achievement types and interests

---
+++ constants/achievement_type.py
@@ -0,0 +1,19 @@
+achievement_type = {
+    0: 'Data Science',
+    1: 'Hackathon',
+    2: 'Artificial Intelligence',
+    3: 'Capture The Flag',
+    4: 'Business Case',
+    5: 'Business Proposal',
+    6: 'Analytics',
+    7: 'UI/UX',
+    8: 'Competitive Programming',
+    9: 'Olympiad',
+    10: 'Game Development',
+    11: 'Scientific Paper',
+    12: 'Internet of Things',
+    13: 'Animation',
+    14: 'Smart City'
+}
+
+MAX_ACHIEVEMENT_TYPE_ID = max(achievement_type.keys())
---
+++ constants/interest.py
@@ -0,0 +1,25 @@
+interest = {
+    0: 'Data Science',
+    1: 'Data Analytics',
+    2: 'Data Engineer',
+    3: 'Artificial Intelligence',
+    4: 'Machine Learning',
+    5: 'AI Research',
+    6: 'Product',
+    7: 'UI/UX',
+    8: 'Front-End Engineer',
+    9: 'Backend Engineer',
+    10: 'DevOps',
+    11: 'Network Security',
+    12: 'Game Development',
+    13: 'Mobile Development',
+    14: 'Marketing',
+    15: 'Partnership',
+    16: 'Business',
+    17: 'Hackathon',
+    18: 'Web3',
+    19: 'Event',
+    20: 'Competitive Programming'
+}
+
+MAX_INTEREST_ID = max(interest.keys())
